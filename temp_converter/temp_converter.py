4f317da4a39874d45d261c6c45c6abb58015ae45
CatSnatcher3107 <seb51@sunyorange.edu>
2024-09-10 13:06:02 -0400

    Updated temp_converter.py
    I added parenthesis in line 6 for this code so the conversion could run properly.
    
    I also adjusted the print statement so that the result would have a unit to symbolize Fahrenheit.
    
    I then added comments to describe which was input/output and what the units were and described what I did in the code itself.

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
-# process
+"""
+Sophia Brady, CSC 138 EG, September 10th, 2024
+"""
 def c2f(c):
-    return c * 9 / 5 + 32
-
+    return (c * 9 / 5) + 32
+#this is the formula used to convert Celsius to Fahrenheit
 def main(cel):
     return c2f(cel)
 
 if __name__ == "__main__":
-    cel = 100         # input
-    print(main(cel))  # output
+    cel = 100         # input (Celsius is set to 100 degrees)
+    print(f"{main(cel)}F")  # output (Fahrenheit, I made the code to print F to symbolize that the result of this code was in Fahrenheit)
